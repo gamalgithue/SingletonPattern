cd13637c5a5e87617d74a0c89a4796c70e0d70da
gamalgithue <gamal_678a@fci.kfs.edu.eg>
2023-11-29 15:30:31 +0200

    first commit

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Linq.Expressions;
+using System.Data.Sql;
+using System.Reflection.PortableExecutable;
+using System.Xml.Linq;
+
+
+namespace ConsoleApp18
+{
+     class connect
+    {
+        private static connect _Instance;
+        private readonly SqlConnection conn = new SqlConnection(@"Data Source=JOO-PC\MSSQLSERVER2022;Initial Catalog=School;Integrated Security=True");
+        private connect()
+        {
+
+        }
+        public static connect GetInstance()
+        {
+            if (_Instance == null)
+            {
+                _Instance = new connect();
+            }
+            return _Instance;
+        }
+        public void GetConnection()
+        {
+            string query = @"select Id as id,FullName as Name , email as Email , BirthDate as BoD from employee;";
+            SqlCommand cmd = new SqlCommand(query, conn);
+
+            conn.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+
+                    string empname = dr["Name"].ToString();
+                    string empgmail = dr["Email"].ToString();
+                    string empdate = dr["BoD"].ToString();
+                    Console.Write($"Name: {empname} and Gmail:{empgmail} and  BOD: {empdate} " + " ");
+
+                    Console.WriteLine();
+                }
+
+            }
+
+            else
+            {
+                Console.WriteLine("no data found");
+            }
+
+            dr.Close();
+            conn.Close();
+        }
+
+    }
+
+
+
+    class program
+    {
+       static void Main(string[] args)
+        {
+            
+            connect c1 = connect.GetInstance();
+            c1.GetConnection();
+        }
+    }
+}
+    
---
+++ SingletonCountVisitor/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading.Tasks;
+using System.Text;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ConsoleApp17
+{
+
+
+    class Visitor
+    {
+        private static Visitor _instance;
+        public static int countvisitor;
+        private Visitor()
+        {
+            countvisitor = 0;
+        }
+        public static Visitor getInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Visitor();
+
+            }
+            countvisitor++;
+
+            return _instance;
+
+        }
+        public void IncrementVisitor()
+        {
+            countvisitor++;
+            Console.WriteLine("Visitor count increased to: " + countvisitor);
+
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Visitor s1 = Visitor.getInstance();
+            Visitor s2 = Visitor.getInstance();
+            Visitor s3 = Visitor.getInstance();
+
+
+            Console.WriteLine("current counter is: " + Visitor.countvisitor);
+
+
+
+
+        }
+    }
+}
